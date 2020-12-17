87a18b1c5057346a401ef71ff93c93713f983efa
mvladzhova <mariya_ladzhova@abv.bg>
2020-12-17 12:10:11 +0000

    commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("kpmg.home.sports_ml1.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
